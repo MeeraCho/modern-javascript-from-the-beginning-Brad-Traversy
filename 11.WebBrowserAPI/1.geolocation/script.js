7ea67c58d66ba46abfa4aea9611cc20ec5b83e2a
MiraCho <miracho119@gmail.com>
2024-03-22 23:30:31 -0600

    added map-position

--- 11.WebBrowserAPI/1.geolocation/script.js
+++ 11.WebBrowserAPI/1.geolocation/script.js
@@ -1,11 +1,9 @@
-// getCurrentPosition()
-
+//------------------getCurrentPosition()---------------------
 function curSuccess(pos) {
   const coords = pos.coords;
-
-  console.log(`Latitude: ${coords.latitude}`);
-  console.log(`Longitude: ${coords.longitude}`);
-  console.log(`Within: ${coords.accuracy} meters`);
+  console.log(`Latitude: ${coords.latitude}`)
+  console.log(`Longitude: ${coords.longitude}`)
+  console.log(`Within: ${coords.accuracy} meters`)
 }
 
 function curError(err) {
@@ -13,31 +11,26 @@ function curError(err) {
 }
 
 const curOptions = {
-  enableHighAccuracy: true, // Use GPS if available
+  enableHighAccuracy: true, // Use GPS if available 
   timeout: 5000, // Time to wait to stop trying for location
-  maximumAge: 0, // Do not use a cached position
+  maximumAge: 0 //Do not use a cached position
 };
 
-// navigator.geolocation.getCurrentPosition(curSuccess, curError, curOptions);
 
-// watchPosition()
+navigator.geolocation.getCurrentPosition(curSuccess, curError, curOptions);
 
+
+//------------------watchPosition()---------------------
 const target = {
   latitude: 41.3874387,
   longitude: -71.394839,
-};
+}
 
 function watchSuccess(pos) {
   const coords = pos.coords;
-  console.log(coords);
-
-  if (
-    target.latitude === coords.latitude &&
-    target.longitude === coords.longitude
-  ) {
-    console.log('You have reached your destination!');
-    navigator.geolocation.clearWatch(id);
-  }
+  console.log(`Latitude: ${coords.latitude}`)
+  console.log(`Longitude: ${coords.longitude}`)
+  console.log(`Within: ${coords.accuracy} meters`)
 }
 
 function watchError(err) {
@@ -45,13 +38,14 @@ function watchError(err) {
 }
 
 const watchOptions = {
-  enableHighAccuracy: true, // Use GPS if available
-  timeout: 5000, // Time to wait to stop trying for location
-  maximumAge: 0, // Do not use a cached position
+  enableHighAccuracy: true,
+  timeout: 5000, 
+  maximumAge: 0 
 };
 
-const id = navigator.geolocation.watchPosition(
-  watchSuccess,
-  watchError,
-  watchOptions
-);
+
+navigator.geolocation.watchPosition(watchSuccess, watchError, watchOptions);
+
+
+
+
